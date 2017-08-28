7a4f6a1ea3925ddeaa655676bb3716ad7abb2572
Prima Aulia Gusta <primaulia@gmail.com>
2017-08-28 15:43:39 +0800

    latest layout

--- src/App.js
+++ src/App.js
@@ -6,13 +6,61 @@ class App extends Component {
   render () {
     return (
       <div className='App'>
-        <div className='App-header'>
+        <h1>Welcome to React</h1>
+        <p>
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+        </p>
+
+        <div className='box'>
+          first
+        </div>
+        <div className='box'>
+          second
+        </div>
+        <div className='box'>
+          third
+        </div>
+
+        <div className='Article-container'>
+          <article className='long'>
+            <h2>My Article</h2>
+            <p>
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+            </p>
+          </article>
+
+          <article>
+            <h2>Second Article</h2>
+            <p>
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </p>
+          </article>
+
+          <article>
+            <h2>Third Article</h2>
+            <p>
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </p>
+          </article>
+
+          <article className='long'>
+            <h2>Fourth Article</h2>
+            <p>
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+            </p>
+          </article>
+
+        </div>
+
+        {/* <div className='App'>
+          <div className='App-header'>
           <img src={logo} className='App-logo' alt='logo' />
           <h2>Welcome to React</h2>
-        </div>
-        <p className='App-intro'>
+          </div>
+          <p className='App-intro'>
           To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+          </p>
+        </div> */}
       </div>
     )
   }
